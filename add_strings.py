2b62dac989975023840f9d9fd2af77c499e0682b
Justin <jsarraga@gmail.com>
2020-02-14 13:45:49 -0500

    add strings

---
+++ add_strings.py
@@ -0,0 +1,5 @@
+def addStrings(num1, num2):
+        # what data type would you like back?
+        # since num1 and num2 are strings, we must convert them to integers to perform any type of math on them
+        # because you want a string returned, we have to wrap the solution in str() to convert it back
+        return str(int(num1) + int(num2))
--- most_common_word.py
+++ most_common_word.py
@@ -1,39 +1,39 @@
 import string
 def mostCommonWord(paragraph, banned):
     """
-        convert to lower
-        remove punctuation
-        create list of words
-        count words
-        find most common
-        """
+    convert to lower
+    remove punctuation
+    create list of words
+    count words
+    find most common
+    """
+    
+    # convert to lower
+    newString = paragraph.lower()
+    
+    #remove punctutation
+    for c in string.punctuation:
+        newString = newString.replace(c, " ")
         
-        # convert to lower
-        newString = paragraph.lower()
-        
-        #remove punctutation
-        for c in string.punctuation:
-            newString = newString.replace(c, " ")
-            
-        
-        word_dict = {}
-        count = 0
-        result = ""
-        
-        # create a list of words
-        for word in newString.split():
-            # bc we replaced punc with " ", we skip " "
-            if word not in banned and word != " ":
-                # keep track of count in dict
-                if word in word_dict:
-                    word_dict[word] += 1
-                else:
-                    word_dict[word] = 1
-                    # most occured tracked by count and result
-                if word_dict[word] > count:
-                    count = word_dict[word]
-                    result = word
-        return result
+    
+    word_dict = {}
+    count = 0
+    result = ""
+    
+    # create a list of words
+    for word in newString.split():
+        # bc we replaced punc with " ", we skip " "
+        if word not in banned and word != " ":
+            # keep track of count in dict
+            if word in word_dict:
+                word_dict[word] += 1
+            else:
+                word_dict[word] = 1
+                # most occured tracked by count and result
+            if word_dict[word] > count:
+                count = word_dict[word]
+                result = word
+    return result
 
 
         
