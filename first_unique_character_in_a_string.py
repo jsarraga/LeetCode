d557c22c3e5b69ea97c6a10f6209cc449c91a191
Justin <jsarraga@gmail.com>
2020-02-18 13:34:03 -0500

    first unique character in a string

---
+++ first_unique_character_in_a_string.py
@@ -0,0 +1,25 @@
+def firstUniqChar(s):
+    char_map = {}
+    for c in s:
+        if c in char_map.keys():
+            char_map[c] += 1
+        else:
+            char_map[c] = 1
+            
+    for i in range(len(s)):
+        if char_map[s[i]] == 1:
+            return i
+    
+    return -1
+
+#slightly faster way using Counter()
+
+from collections import Counter
+def firstUniqChar1(s):
+    char_count = Counter(s)
+
+    for i in range(len(s)):
+        # .get() returns the value of given key s[i]
+        if char_count.get(s[i]) == 1:
+            return i
+    return -1
