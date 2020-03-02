521ab04aba9df114d737742f2d2a02268d6da531
Justin <jsarraga@gmail.com>
2020-03-02 15:56:39 -0500

    same tree

---
+++ same_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        output = []
+        p = self.preorder(p, output)
+        q = self.preorder(q, output)
+        return p == q
+        
+    #  traverse the tree in preorder (root, left, right)
+    # then compare the two
+    def preorder(self, root, output):
+        output = []
+        self.helper(root, output)
+        return output
+        
+    def helper(self, root, output):
+        if not root:
+            output.append("None")
+        else:
+            output.append(root.val)
+            self.helper(root.left, output)
+            self.helper(root.right, output)
