ffd00ebedbb434b842b9ccefae8dc515ae72d85c
Justin <jsarraga@gmail.com>
2020-03-11 16:30:58 -0400

    intersection of two arrays with notes and alternate solutions

--- intersection_of_two_arrays.py
+++ intersection_of_two_arrays.py
@@ -2,9 +2,24 @@ class Solution:
     # faster solution: using pythons intersection() function. Takes in two sets. -- 44 ms
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return set(nums1).intersection(set(nums2))
-    
-# make a set of the results. set's rule out duplicates -- 72 ms
+
+    # alternate solution using hashmap and sets
     def intersection1(self, nums1, nums2):
+        hashmap = {}
+        result = set()
+
+        for x in nums1:
+            if x not in hashmap:
+                hashmap[x] = True
+
+        for y in nums2:
+            if y in hashmap:
+                result.add(y)
+        
+        return result
+    
+    # make a set of the results. set's rule out duplicates -- 72 ms
+    def intersection2(self, nums1, nums2):
         result = set()
         for i in nums1:
             if i in nums2:
