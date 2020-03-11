70b2899687db72b9c2f02b95c2c8d37b32c0fed2
Justin <jsarraga@gmail.com>
2020-03-11 16:24:35 -0400

    intersection of two arrays

---
+++ intersection_of_two_arrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    # faster solution: using pythons intersection() function. Takes in two sets.
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1).intersection(set(nums2))
+    
+# make a set of the results. set's rule out duplicates
+    def intersection1(self, nums1, nums2):
+        result = set()
+        for i in nums1:
+            if i in nums2:
+                result.add(i)
+        return result
