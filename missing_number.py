2dff384ccbdd67699a5c9cbff06ae84e6eeb70bb
Justin <jsarraga@gmail.com>
2020-03-18 17:31:11 -0400

    missing number

--- missing_number.py
+++ missing_number.py
@@ -1,3 +1,6 @@
+# 1. Get the sum of numbers which is total = n*(n+1)/2 --> Gauss' formula
+# 2. Subtract all the numbers from sum and you will get the missing number
+
 def missingNumber(nums):
     n = len(nums)
     total = n*(n+1) / 2
