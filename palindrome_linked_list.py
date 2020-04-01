ff3b5776240aab79dd65833a6a3e9a7a2ee6ad19
Justin <jsarraga@gmail.com>
2020-04-01 12:20:09 -0400

    palindrome linked list - correct way

--- palindrome_linked_list.py
+++ palindrome_linked_list.py
@@ -1,7 +1,5 @@
-# Approach:
-# make a list of the the linked list values and check if the reverse == original
 
-
+# cheater approach
 def isPalindrome(head):
     result = []
     while head:
@@ -9,3 +7,35 @@ def isPalindrome(head):
         head = head.next
 
     return result == result[::-1]
+
+# Approach:
+# find the midpoint
+# reverse the second half of the linked list
+# check if first half == second half while traversing them
+
+def isPalindrome1(head):
+    # find the mid node -> bc fast is going twice as fast as slow, when fast hits the end, slow will hit the middle
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        
+    # reverse the second half -> slow is head
+    node = None
+    while slow:
+        ptr = slow.next
+        slow.next = node
+        node = slow
+        slow = ptr
+        
+    
+    # compare the first and second half nodes
+    while node:
+        if head.val != node.val:
+            return False
+        else:
+            head = head.next
+            node = node.next
+    return True
