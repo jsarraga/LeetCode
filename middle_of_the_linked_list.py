0cacb0b268d5e81f4a22498090c51ebcc39955b4
Justin <jsarraga@gmail.com>
2020-04-03 14:43:53 -0400

    middle of linked list. output to an array

--- middle_of_the_linked_list.py
+++ middle_of_the_linked_list.py
@@ -2,8 +2,8 @@
 # have one pointer (fast) go through the list twice as fast as the the other (slow)
 # by the time the fast pointer hits the end of the list, slow will reach the middle node. return slow
 
+# Two - pointer method
 def middleNode(head):
-
     slow = head
     fast = head
 
@@ -12,3 +12,11 @@ def middleNode(head):
         fast = fast.next.next
 
     return slow
+
+# output to an array
+def middleNode1(head):
+    result = [head]
+        
+    while result[-1].next:
+        result.append(result[-1].next)
+    return result[len(result) // 2]
